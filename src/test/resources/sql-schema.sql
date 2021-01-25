0cc53074512072ed50fed0a6d982c5a3cc63e81c
SehunB-QA <sbabatunde@qa.com>
2021-01-25 14:06:30 +0000

    SIA-50 : JUnit tested all of Item's and customer's CRUD functions with 100% test coverage. Added items table sql and a insert into tables sql query in respective test .sql files. Added screenshots of the JUnit tests & test coverage of customer and items.

--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -6,3 +6,13 @@ CREATE TABLE IF NOT EXISTS `customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+DROP TABLE IF EXISTS `items`;
+
+CREATE TABLE IF NOT EXISTS `items`(
+  `id_items` INT NOT NULL AUTO_INCREMENT,
+  `item_name` VARCHAR(100) NULL,
+  `item_price` DECIMAL(9,2) NULL,
+  PRIMARY KEY (`id_items`)
+  );
+
